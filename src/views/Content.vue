8c39a1ba4d39c5119223121b6143043c4df88915
TheNerdyGamerOMG <61297975+TheNerdyGamerOMG@users.noreply.github.com>
2024-09-28 22:02:48 -0400

    initial content page

--- src/views/Content.vue
+++ src/views/Content.vue
@@ -1,14 +1,51 @@
 <template>
-  <div class="content">
-    <div class="form-group">
-      <input placeholder="Search...." v-model="searchQuery" required/>
-      <button @click="search"></button>
+  <div id="parent">
+    <div id="table-of-content">
+      SHOULD BE A VERTICAL LIST FULL OF TITLED
+    </div>
+    <div id="content"> 
+      SHOULD BE RENDERED USING TEXME
     </div>
   </div>
 </template>
 
 <script>
-
+export default {
+    data() {
+      //Data shoul come from pinia add a method to the global store thats fetches whatever is given from the search menu
+    }
+}
 </script>
 
-<style></style>
+<style>
+#app:has(> #parent) {
+  padding: 0;
+  margin: 0;
+  overflow-x: hidden;
+}
+body:has(#parent) {
+  margin: 10px;
+  margin-bottom: 0;
+}
+</style>
+
+<style scoped>
+#parent {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+}
+#table-of-content {
+  border-right: 1rem solid black;
+  height: inherit;
+  width: 40%;
+}
+#content {
+  border-left: 1rem solid black;
+  height: inherit;
+  width: 60%;
+}
+</style>
